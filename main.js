fefa15afc210085f07f0dd26f4b1551fb1e66cd9
Anthony <tonjoe99@gmail.com>
2020-12-08 15:27:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// declare all the shapes here by Id
+let box1 = document.getElementById("box1");
+
+window.addEventListener("scroll", function(event) {
+  let top = this.scrollY;
+
+  console.log(top);
+  if(top > 450) {
+    box1.classList.add("scale-animation");
+  } else {
+    box1.classList.remove("scale-animation");
+  }
+}, false);
